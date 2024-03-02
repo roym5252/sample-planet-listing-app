baebe8f868d8b3401dba0db876e0036ac446af45
roymathew <roym5252@gmail.com>
2024-03-02 13:28:00 +0000

    - Pass 'check loader is displayed' test by adding basic loader.

--- feature-planet-list/src/main/java/com/jpm/feature_planet_list/PlanetListScreen.kt
+++ feature-planet-list/src/main/java/com/jpm/feature_planet_list/PlanetListScreen.kt
@@ -1,9 +1,11 @@
 package com.jpm.feature_planet_list
 
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 
 @Composable
 fun PlanetListScreen() {
-
-
+    CircularProgressIndicator(modifier = Modifier.testTag("planetListLoader"))
 }
